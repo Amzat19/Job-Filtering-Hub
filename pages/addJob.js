54a28c6a81d0f95cafa286d95f2344722caf2116
Sheriff Amzat <kunmiamzat1@gmail.com>
2022-07-05 15:15:14 +0100

    Fixed Head for pages

--- pages/addJob.js
+++ pages/addJob.js
@@ -2,6 +2,16 @@ import AddNewJob from "../components/AddNewJob";
 
 export default function addJob() {
     return (
+        <>
+        <Head>
+            <title>Add New Job</title>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content='A Nextjs website built to show available job listings with filter functionality' />
+            <meta name="robots" content="index, nofollow"/>
+            <meta name="viewport" content='width=device-width, initial-scale=1'/>
+            </Head>
         <AddNewJob/>
+        </>
+        
     )
 }
